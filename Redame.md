b58c947f5a54c3d69fd368f24a4c55835bf5d8ce
Celio Junior <84292058+AIemao@users.noreply.github.com>
2022-01-15 11:06:16 -0300

    SUPERGIG

---
+++ Redame.md
@@ -0,0 +1,25 @@
+Projeto em AULA SENAC.
+
+SUPERGIG é um projeto de aplicatico de gerenciamentos de bandas e eventos.
+
+Servidor (API)
+Javascript
+NodeJS
+Sequelize
+Express
+
+ADM
+Javascript
+NodeJS
+Vue
+
+Site
+Javascript
+NodeJS
+Vue
+
+APP
+Javascript
+NodeJS
+Quasar
+Vue
---
+++ src/index.js
@@ -0,0 +1,15 @@
+var express = require("express")
+var app = express()
+
+app.get('/', function (req, res){
+    res.send('SUPERGIG API');
+});
+
+app.all("*", function(req, res, next) {
+    console.log("Acessou a pagina")
+    next();
+});
+
+app.listen(3000, function(){
+    console.log('Servidor rodando na porta 3000');
+});
